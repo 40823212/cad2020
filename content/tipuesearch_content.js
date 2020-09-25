416a3cc149057bcd6802c7ea693ee5c7e1c3446f
40823212 <40823212@gm.nfu.edu.tw>
2020-09-25 16:32:34 +0800

    使用SSH

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/40823212/cad2020 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/40823212/cad2020 \n', 'tags': '', 'url': 'About.html'}, {'title': 'soildwork PDF.1', 'text': '', 'tags': '', 'url': 'soildwork PDF.1.html'}]};
