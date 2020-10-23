755f356deffeb6d05ebf5caad65e6ad90b1fac01
40823212 <40823212@gm.nfu.edu.tw>
2020-10-23 14:09:25 +0800

    12

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/40823212/cad2020 \n', 'tags': '', 'url': 'About.html'}, {'title': 'soildwork PDF.1', 'text': '', 'tags': '', 'url': 'soildwork PDF.1.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/40823212/cad2020 \n', 'tags': '', 'url': 'About.html'}, {'title': 'soildwork PDF.1', 'text': '\xa0 \xa0 \xa0 \xa01. \n \n 2. \n 3.', 'tags': '', 'url': 'soildwork PDF.1.html'}]};
